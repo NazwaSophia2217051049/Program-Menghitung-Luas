2e3dc9f3a3d0f953233be3cc50dd2da4e0f7392a
NazwaSophia2217051049 <112567851+NazwaSophia2217051049@users.noreply.github.com>
2022-09-08 22:18:36 -0700

    Add files via upload

---
+++ 2217051049_Nazwa Sophia N.E_Luas lingkaran.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+	float luas, phi=3.14;
+	int r;
+	
+	cout<<"Masukan Jari-Jari = ";
+	cin>>r;
+	
+	luas=phi*r*r;
+	
+	cout<<"Luas Lingkaran adalah = "<<luas<<endl;
+	getch();
+	
+	return 0;
+
+}
